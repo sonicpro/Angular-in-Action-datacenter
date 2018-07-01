a0f515cdcd71c84ab6598868b7d5ea7d3195ab0a
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2018-07-01 13:25:17 +0300

    Accessors added to the "display" component to sanitize input.

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -6,11 +6,30 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./metric.component.css']
 })
 export class MetricComponent {
+    private valueField: number = 0;
+    private maxField: number = 100;
+  
     @Input() public title: string = "";
     @Input() public description: string = "";
-    @Input("used") public value: number = 0;
-    @Input("available") public max: number = 100;
+    @Input("used")
+    public set value(value: number){
+      if (isNaN(value)) {
+        value = 0;
+      }
+      this.valueField = value;
+    }
+    public get value(): number { return this.valueField; }
 
+    @Input("available")
+    public set max(max: number){
+      if (isNaN(max)) {
+        max = 0;
+      }
+      this.maxField = max;
+    }
+    public get max(): number { return this.maxField; }
+
+    // This member now protected from invalid inputs by means of accessors.
     isDanger() {
       return this.value / this.max > 0.7;
     }
